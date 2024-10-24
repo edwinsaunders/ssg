fac732c1bfe3c4852d633ee9d92600f256a16d91
edwinsaunders <edwinsaunders36@gmail.com>
2024-10-23 23:43:00 -0400

    7. Implement parent nodes within htmlnode.py and added corresponding unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,7 +29,30 @@ class LeafNode(HTMLNode):
 
 	def to_html(self):
 		if not self.value:
-			raise ValueError
+			raise ValueError('no value')
 		if not self.tag:
 			return self.value
 		return f"<{self.tag}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+	def __init__(self, tag=None, children=None, props=None):
+				
+		super().__init__(tag, None, children, props)
+
+	def to_html(self):
+		
+		if not self.tag:
+			raise ValueError('no tag')
+		if not self.children:
+			raise ValueError('no children')
+		
+
+		string = ""
+		for child in self.children:
+			string += child.to_html()
+
+		return f"<{self.tag}>{string}</{self.tag}>"
+		
+
+
+			#return f"<{self.tag}>{self.children[0].to_html(children[:1])}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,11 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
+
+    #base class tests
     def test_props_to_html_generic(self):
         node1 = HTMLNode()
         node2 = HTMLNode()
@@ -30,6 +32,8 @@ class TestHTMLNode(unittest.TestCase):
         string = ""
         self.assertEqual(node.props_to_html(), string)
 
+    
+    #LeafNode tests
     def test_to_html_typical(self):
         node = LeafNode(
             "p", "this is a\nparagraph\n", 
@@ -66,6 +70,85 @@ class TestHTMLNode(unittest.TestCase):
         
         test_string = 'this is a\nparagraph\n'
         self.assertEqual(node.to_html(), test_string)
+
+
+    #ParentNode tests
+    def test_to_html_oneChild(self):
+        child = LeafNode(
+            "b", "this is a\nparagraph\n", 
+            None, 
+            {
+            'href': 'http://www.x.com', 
+            'blah': 'poopoo', 'third': 'i dunno'
+            }
+            )
+        node = ParentNode(
+            "p", 
+            [child], 
+            {
+            'href': 'http://www.x.com', 
+            'blah': 'poopoo', 'third': 'i dunno'
+            }
+            )
+        test_string = '<p><b>this is a\nparagraph\n</b></p>'
+        self.assertEqual(node.to_html(), test_string)
+
+    def test_to_html_nestedParents(self):
+        leaf = LeafNode(
+            "b", "this is a\nparagraph\n", 
+            None, 
+            {
+            'href': 'http://www.x.com', 
+            'blah': 'poopoo', 'third': 'i dunno'
+            }
+            )
+        parent1 = ParentNode(
+            "p", 
+            [leaf], 
+            {
+            'href': 'http://www.x.com', 
+            'blah': 'poopoo', 'third': 'i dunno'
+            }
+            )
+        node = ParentNode(
+            "h", 
+            [parent1], 
+            {
+            'href': 'http://www.x.com', 
+            'blah': 'poopoo', 'third': 'i dunno'
+            }
+            )
+        test_string = '<h><p><b>this is a\nparagraph\n</b></p></h>'
+        self.assertEqual(node.to_html(), test_string)
+
+    def test_to_html_complex(self):
+
+        lnodeB2 = LeafNode("d", "blah", None, None)
+
+        lnodeB1 = LeafNode("c", "blahblah", None, None)
+
+        pnodeB  = ParentNode("a", [lnodeB1, lnodeB2], None)
+
+        lnodeA1 = LeafNode("b", "this is a\nparagraph\n", None, None)
+
+        pnodeA = ParentNode("p", [lnodeA1], None)
+            
+        masternode = ParentNode("h", [pnodeA, pnodeB], None)
+
+        test_string = '<h><p><b>this is a\nparagraph\n</b></p><a><c>blahblah</c><d>blah</d></a></h>'
+
+        self.assertEqual(masternode.to_html(), test_string)
+
+    def test_to_html_noChildren(self):
+        node = ParentNode("p", None, None)
+        
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_Tag(self):
+        node2 = LeafNode("d", "blah", None, None)
+        node = ParentNode(None, [node2], None)
+        
+        self.assertRaises(ValueError, node.to_html)
 
 if __name__ == "__main__":
     unittest.main()
